310f52b06b26b4753eb305c49fa4e2ef72852287
jonguman29-debug <jon.gu.man.29@gmail.com>
2026-08-24 15:17:47 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,38 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+
 const firebaseConfig = {
+
   apiKey: "AIzaSyAY0ywZLUKj8UoxuVRJQS0oLsZ9QWUR000",
+
   authDomain: "equeb-b79a9.firebaseapp.com",
+
   projectId: "equeb-b79a9",
+
   storageBucket: "equeb-b79a9.firebasestorage.app",
+
   messagingSenderId: "602534699081",
+
   appId: "1:602534699081:web:ecc26cf4c8557fd9159f98"
+
 };
+
 
 const app = initializeApp(firebaseConfig);
 
-export { app };
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+
+export {
+  app,
+  auth,
+  db
+};
